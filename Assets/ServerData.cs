9a9e307baf3b3ca878dd152b6e95034c5dbb2005
kaifarick <mesqueinbarca@mail.ru>
2020-09-02 21:57:02 +0300

    ServerData

---
+++ Assets/ServerData.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using UnityEngine.Networking;
+using UnityEngine;
+using System.Collections.Generic;
+
+namespace WeeklyAtivity
+{
+    public class ServerData : MonoBehaviour
+    {
+        private string jsonUrl;
+
+        public int[] Calories = new int[7];
+        void Start()
+        {
+            jsonUrl = "http://n44.me/statistics.json";
+            StartCoroutine(LoadTextFromServer());
+        }
+
+        IEnumerator LoadTextFromServer()
+        {
+            UnityWebRequest request = UnityWebRequest.Get(jsonUrl);
+            yield return request.SendWebRequest();
+            if (request.isNetworkError || request.isHttpError)
+            {
+                Debug.Log(request.error);
+            }
+            else
+            {
+                JsonData(request.downloadHandler.text);
+            }
+        }
+        void JsonData(string json)
+        {
+            AllStatistic AllStats = JsonUtility.FromJson<AllStatistic>(json);
+
+            foreach (ActData Value in AllStats.data)
+            {
+                Debug.Log(Value.value);
+            }
+        }
+
+
+        [System.Serializable]
+
+        public class AllStatistic
+        {
+            public int previous;
+            public int current;
+            public int total;
+            public List<ActData> data;
+        }
+
+        [System.Serializable]
+        public class ActData
+        {
+            public int value;
+        }
+    }
+}
---
+++ Assets/StatisticsChart.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Linq;
+
+namespace WeeklyAtivity
+{
+
+    public class StatisticsChart : MonoBehaviour
+    {
+        int[] calories = new int[7];
+        Slider[] sliders = new Slider[7];
+
+        int lenght = 0;
+
+        void Start()
+        {
+            sliders = GetComponentsInChildren<Slider>();
+
+            RandomData();
+            WithMax();
+        }
+
+        void WithoutMax()
+        {
+            int max = calories.Max();
+
+            for (int i = 0; i < sliders.Length; i++, lenght++)
+            {
+                sliders[lenght].maxValue = max;
+                sliders[lenght].value = calories[lenght];
+                
+            }
+            
+        }
+        void WithMax()
+        {
+            for (int i = 0; i < sliders.Length; i++, lenght++)
+                sliders[lenght].value = calories[lenght];
+        }
+
+        void RandomData()
+        {
+            for (int i = 0; i < calories.Length; i++)
+                calories[i] = Random.RandomRange(0, 5000);;
+        }
+    }
+}
